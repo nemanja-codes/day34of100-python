f585bed78dcad69a06ed314489d91b1c4c5083cd
Nemanja Markovic <necamark@gmail.com>
2024-06-04 16:52:08 +0200

    "created class based UI"

--- ui.py
+++ ui.py
@@ -1,2 +1,33 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score = 0
+        self.score_label = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white", font=("Arial", 15, "normal"))
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(height=250, width=300, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some Question Text",
+            fill="black",
+            font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_btn = Button(image=true_img, highlightthickness=0)
+        self.true_btn.grid(row=2, column=0)
+        false_img = PhotoImage(file="images/false.png")
+        self.false_btn = Button(image=false_img, highlightthickness=0)
+        self.false_btn.grid(row=2, column=1)
+
+        self.window.mainloop()
